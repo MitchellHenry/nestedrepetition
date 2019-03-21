e8a0585135353c6378caf547259ebeaaf27c3c2a
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2019-03-21 15:39:48 +1100

    initial

---
+++ NestedRepetition/Exercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mitchell
+ * Date: 21/03/2019
+ * Time: 3:33 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Exercise2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
